92e0be06e6b2241bde4f9415b32df85deca463a2
crosslock <kossqwas@gmail.com>
2016-06-18 16:55:58 +0800

    fix ndk setting

--- app/src/main/jni/hello-jni.c
+++ app/src/main/jni/hello-jni.c
@@ -12,7 +12,6 @@ jstring Java_com_ork_ndkdemo_MainActivity_add
 jstring Java_com_ork_ndkdemo_MainActivity_add2(JNIEnv *env,jint i,jint j){
    char* str = malloc(16);
    int res;
-
    res = 3 + 4;
    snprintf(str, 16, "%d", res);
    return (*env)->NewStringUTF(env,str);
